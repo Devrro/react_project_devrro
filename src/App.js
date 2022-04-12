dd17d35fc3a66f134bdb61c69d3e812d3a0043f2
Serhii_Terletckiy <beinght@gmail.com>
2022-04-12 12:43:51 +0300

    Lesson 1 Homework

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import Comments from "./components/Comments";
-import Users from "./components/Users";
-import Posts from "./components/Posts";
-
-    let urlComments = 'https://jsonplaceholder.typicode.com/comments'
-let urlPosts = 'https://jsonplaceholder.typicode.com/posts'
-let urlUsers = 'https://jsonplaceholder.typicode.com/users'
 
 function App() {
   return (
       <div className={'main_container'}>
-        <Users url={`${urlUsers}`}/>
-        <Posts url={`${urlPosts}`}/>
-        <Comments url={`${urlComments}`}/>
+
       </div>
   )
 

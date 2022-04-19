fd74563bc0a899a7d387a8f696a18514cefcd7a5
Serhii_Terletckiy <beinght@gmail.com>
2022-04-19 15:56:57 +0300

    Lesson5_hw

--- src/services/post.service.js
+++
@@ -1,9 +0,0 @@
-import axiosInstance from "./axios.service";
-import {urls} from "../constants";
-
-const postService = {
-    getAll: () => axiosInstance.get(urls.posts),
-    getById: (id) => axiosInstance.get(urls.posts +`/${id}`)
-}
-
-export default postService;

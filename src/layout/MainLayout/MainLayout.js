a4f219fa995775bab7a3a23933ce87ba445b31fd
Serhii_Terletckiy <beinght@gmail.com>
2022-04-20 14:19:43 +0300

    Lesson5_hw_2

--- src/layout/MainLayout/MainLayout.js
+++ src/layout/MainLayout/MainLayout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import style from './MainLayout.module.css'
 import {Link,Outlet} from "react-router-dom";
+import Button from "../../components/Button/Button";
 
 const MainLayout = () => {
     return (
         <div>
             <div className={style.header}>
-                <Link to={'/home'}>Home</Link>
+                <Button>Kot</Button>
+                <Button>Dogo</Button>
             </div>
             <Outlet/>
         </div>
